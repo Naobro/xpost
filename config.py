d0109fcb769187913c208f1ba772001d59db8212
Naobro <naokiwm@icloud.com>
2025-08-02 21:50:26 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ WP_APP_PASSWORD = "j7AqlMnSdvIvdLY7xNVf37sZ"  # スペース削除済み
 # =========================
 # Twitter APIキー
 # =========================
-API_KEY = "6Dt9POUMgE72NbID6ozNzGgrx"
-API_SECRET = "5ksJQgiF4jhaTJjQgdNoCJ98O3DSdORZjvksHiQFSkt7FafwTX"
+API_KEY = "xYGUMu32mK3tGdQiiQ83o8nk3"
+API_SECRET = "zVUl0qk0WrDQlWkX6jSmuu062RcycY6WJvthlV6Kv4a0pcbagi"
 ACCESS_TOKEN = "1285510253212102656-yZUac1dVk2bDHHeaYOQb50TzVy5z22"
 ACCESS_TOKEN_SECRET = "glmAr7FGd1mu7o9g5V0hBmMksMANJM5QHZWEBzpnmgBim"
 
